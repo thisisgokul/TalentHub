0d99814ff95e1e773d4b2acc7a8eb94b130f43ad
thisisgokul <gokulgs4923@gmail.com>
2023-11-11 20:38:28 +0530

    domain add test

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000'
+  origin: 'https://talent-hub-sigma.vercel.app'
 }));
 app.use(cookieparser());
 
